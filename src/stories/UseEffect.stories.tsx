9385f48589fb02952924eae50119622436c4c830
Yujik <yujika87@gmail.com>
2022-03-23 13:30:29 +0300

    UseEffect, setTimeout

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -8,13 +8,47 @@ export default {
 export const Example1 = () => {
     console.log('Example1')
     const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
 
     useEffect(() => {
-        console.log('useEffect')
+        console.log('useEffect every render');
+        document.title = counter.toString();
     })
 
+    useEffect(() => {
+        console.log('useEffect only first render (componentDidMount');
+        document.title = counter.toString();
+    }, [])
+
+    useEffect(() => {
+        console.log('useEffect first render and every counter change');
+        document.title = counter.toString();
+    },[counter])
+
     return <>
-        Hello, {counter}
-        <button onClick={() => setCounter(counter + 1)}>+</button>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>Fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>Counter+</button>
+    </>
+}
+
+export const SetTimeoutExample1 = () => {
+    console.log('SetTimeoutExample1')
+    const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log('setTimeout')
+            document.title = counter.toString();
+        }, 1000)
+    }, [counter])
+
+
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>Fake+</button>
+        <button onClick={() => setCounter(counter + 1)}>Counter+</button>
     </>
 }
